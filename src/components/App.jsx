2308434371dba14dfdaae21bc0596eca15ad1a96
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-10 01:32:41 -0300

    fixed some eslint errors but the oldfamiliars

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,22 @@
-/* eslint-disable react/state-in-constructor  import/no-named-as-default */
+/* eslint-disable react/state-in-constructor,  import/no-named-as-default */
 import React, { Component } from 'react';
 import '../syles/App.css';
-import ButtonPanel from './ButtonPanel'
-import Display from './Display'
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
 
 
 class App extends Component {
-  
   constructor(props) {
     super(props);
     this.status = {
       result: '0',
-    }
+    };
   };
 
   render() {
     const { result } = this.state;
     return (
-      <div className='App'>
+      <div className="App">
         <Display result={result} />
         <div>
           <ButtonPanel />
