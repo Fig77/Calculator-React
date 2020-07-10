5d47c7eb7da3da9927ff92d3f2c7cfcd3457a2c0
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-09 22:20:18 -0300

    changed json

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import App from './components/App';
 import './syles/index.css';
-
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
