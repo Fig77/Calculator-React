bd9a215ea72fb1aa2e4ed701e9c4f5cdb57efc66
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-10 01:24:06 -0300

    fixed = error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/state-in-constructor  import/no-named-as-default */
 import React, { Component } from 'react';
 import '../syles/App.css';
 import ButtonPanel from './ButtonPanel'
@@ -7,20 +7,23 @@ import Display from './Display'
 
 class App extends Component {
   
-state = {
-    result: '0',
-};
+  constructor(props) {
+    super(props);
+    this.status = {
+      result: '0',
+    }
+  };
 
-render() {
-  const { result } = this.state;
-  return (
-    <div className='App'>
-      <Display result={result} />
-      <div>
-        <ButtonPanel />
+  render() {
+    const { result } = this.state;
+    return (
+      <div className='App'>
+        <Display result={result} />
+        <div>
+          <ButtonPanel />
+        </div>
       </div>
-    </div>
-  );
- }
+    );
+  }
 }
 export default App;
