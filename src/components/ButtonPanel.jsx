b3dc7b9e6737673e9d11fccd36291f425d2d3215
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-11 03:15:42 -0300

    button panel styling

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -3,31 +3,31 @@ import Button from './Button';
 
 const ButtonPanel = () => ( 
   <div className="">
-    <div>
+    <div className="group">
       <Button value="AC" />
       <Button value="+" />
       <Button value="-" />
       <Button value="%" />
     </div>
-    <div>
+    <div className="group">
       <Button value="7" />
       <Button value="8" />
       <Button value="9" />
       <Button value="7" />
     </div>
-    <div>
+    <div className="group">
       <Button value="4" />
       <Button value="5" />
       <Button value="6" />
       <Button value="-" />
     </div>
-    <div>
+    <div className="group">
       <Button value="1" />
       <Button value="2" />
       <Button value="3" />
       <Button value="+" />
     </div>
-    <div>
+    <div className="group">
       <Button value="0" />
       <Button value="-" />
       <Button value="=" />
