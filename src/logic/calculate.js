c9b68ac2bb5ec2776af260615856a82ed3f072a2
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-11 04:44:28 -0300

    milestone-4-init

---
+++ src/logic/calculate.js
@@ -0,0 +1,5 @@
+const calculate = ({total, next, operation}, buttonName) => {
+  
+};
+
+export default calculate;
---
+++ src/logic/operate.js
@@ -0,0 +1,13 @@
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  let one = new Big(numberOne);
+  let two = new Big(numberTwo);
+  if (numberTwo === 0) {
+    return 'inf';
+  } else {
+    let result = numberOne.operate(numberTwo);
+    return result;
+  }
+
+};
