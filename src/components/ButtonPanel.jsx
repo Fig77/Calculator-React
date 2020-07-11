154adba2dec30ea5b7e33d946c321ca0f9ec5f0a
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-11 13:11:11 -0300

    added dark orange hex as pass-by variable to some buttons

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -6,33 +6,33 @@ let color = '#ff5722';
 const ButtonPanel = () => ( 
   <div className="">
     <div className="group">
-      <Button value="AC" />
-      <Button value="+/-" />
-      <Button value="÷" />
-      <Button value="%" />
+      <Button value="AC" color={color} />
+      <Button value="+/-" color={color} />
+      <Button value="÷" color={color} />
+      <Button value="%" color={color} />
     </div>
     <div className="group">
       <Button value="7" />
       <Button value="8" />
       <Button value="9" />
-      <Button value="X" />
+      <Button value="X" color={color} />
     </div>
     <div className="group">
       <Button value="4" />
       <Button value="5" />
       <Button value="6" />
-      <Button value="-" />
+      <Button value="-" color={color}/>
     </div>
     <div className="group">
       <Button value="1" />
       <Button value="2" />
       <Button value="3" />
-      <Button value="+" />
+      <Button value="+" color={color} />
     </div>
     <div className="group">
       <Button value="0" wide={wide}/>
       <Button value="." />
-      <Button value="=" />
+      <Button value="=" color={color} />
     </div>
   </div>
 );
