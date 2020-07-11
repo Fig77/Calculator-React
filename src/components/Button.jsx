137cf77ce48f2f01c7a5d8034108495091fd23f4
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-11 02:37:52 -0300

    fixed error on button, added buttonpanel

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({value}) => {
+const Button = ({ value }) => (
   <button type="button" value={value}>{value}</button>
-};
+);
 
 Button.propTypes = {
   value: PropTypes.string.isRequired,
-}
+};
 
 export default Button;
