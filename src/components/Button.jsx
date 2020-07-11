e07a13fbe4ce8f30486ea4a86a340bed5293608c
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-11 01:07:23 -0300

    updating history

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ value }) => (
+  <button type="button" value={value}>{value}</button>
+);
+
+Button.propTypes = {
+  value: PropTypes.string.isRequired,
+};
+
+export default Button;
