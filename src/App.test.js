ee00602fba2bb94b34c479ebeb60068eea5fdc48
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-13 16:18:29 -0300

    esint on apptest

--- src/App.test.js
+++ src/App.test.js
@@ -1,100 +1,95 @@
-import React from 'react';
-import {
-  render
-} from '@testing-library/react';
-import App from './components/App';
 import
 calculate
-from './logic/calculate';
+  from './logic/calculate';
 
 describe('quick calculatortest', () => {
   test('call +/- and get negative if positive', () => {
-    let answ = calculate.calculate({
+    const answ = calculate.calculate({
       total: 10,
       next: '',
-      operation: '+/-'
+      operation: '+/-',
     }, '');
     expect(answ.result.total.toString()).toBe('-10');
   });
 
   test('number/0 should return INF', () => {
-    let answ = calculate.calculate({
+    const answ = calculate.calculate({
       total: 10,
       next: 0,
-      operation: '/'
+      operation: '/',
     }, '=');
     expect(answ.result.total.toString()).toBe('INF');
   });
 
   test('0/0 should return Undefined', () => {
-    let answ = calculate.calculate({
+    const answ = calculate.calculate({
       total: '0',
       next: '0',
-      operation: '/'
+      operation: '/',
     }, '=');
     expect(answ.total.toString()).toBe('Undefined');
   });
 
   test('0 -> operation -> operation should return syntax error', () => {
-    let answ = calculate.calculate({
+    const answ = calculate.calculate({
       total: 0,
       next: '+',
-      operation: '/'
+      operation: '/',
     }, '=');
     expect(answ.result.total.toString()).toBe('Syntax Error');
   });
 
   test('0 -> operation -> operation should return syntax error', () => {
-    let answ = calculate.calculate({
+    const answ = calculate.calculate({
       total: 0,
       next: '+',
-      operation: '/'
+      operation: '/',
     }, '=');
     expect(answ.result.total.toString()).toBe('Syntax Error');
   });
 
   test('number -> null -> = should return the same number', () => {
-    let answ = calculate.calculate({
+    const answ = calculate.calculate({
       total: '34562',
       next: '',
-      operation: '='
+      operation: '=',
     }, '=');
     expect(answ.result.total.toString()).toBe('34562');
   });
 
   test('123 * 2 should return 246', () => {
-    let answ = calculate.calculate({
+    const answ = calculate.calculate({
       total: '123',
       next: '2',
-      operation: '*'
+      operation: '*',
     }, '=');
     expect(answ.result.total.toString()).toBe('246');
   });
 
   test('31 % 3 should return 1', () => {
-    let answ = calculate.calculate({
+    const answ = calculate.calculate({
       total: 31,
       next: '3',
-      operation: '%'
+      operation: '%',
     }, '=');
     expect(answ.result.total.toString()).toBe('1');
   });
 
   test('1234 + -1235 should return 2469', () => {
-    let answ = calculate.calculate({
+    const answ = calculate.calculate({
       total: '1234',
       next: '-1235',
-      operation: '+'
+      operation: '+',
     }, '=');
     expect(answ.result.total.toString()).toBe('-1');
   });
 
   test('9568 + 251 should return 9819', () => {
-    let answ = calculate.calculate({
+    const answ = calculate.calculate({
       total: '9568',
       next: '251',
-      operation: '+'
+      operation: '+',
     }, '=');
     expect(answ.result.total.toString()).toBe('9819');
   });
-})
+});
