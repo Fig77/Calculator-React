4c3ef4253a66b47d3036accaa68894f447c55146
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-13 13:36:16 -0300

    basic milestone-4 template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,12 @@ import Display from './Display';
 import calculate from '../logic/calculate';
 
 const App  = () => {
-  let total = ''; // by def should be always 0 no matter what
-  let next = '';
+  let total = '='; // by def should be always 0 no matter what
+  let next = '=';
   let operation = '+';
   let culator = {total, next, operation};
   let tt = calculate(culator, '=');
-  console.log(tt).newTotal.toString();
+  console.log(tt)
 return (
   <div className="App d-flex flex-column justify-c-center align-i-center">
     <Display />
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,68 +1,66 @@
 import operate from './operate';
 
-const cases = () => ['Syntax Error', 'INF'];
 
-const calculate = ({
-  total,
-  next,
-  operation,
-}, buttonName) => {
-  let newTotal = total;
-  let newOperation = operation;
-  let newNext = next;
-
-  if (next === '' && operation !== '') {
-    if (operation === '+/-' || operation === 'AC' || operation === '=') {
-      return [cases[0]];
+const errorCheck = (one, two, operation) => {
+  if (one === '' && operation !== '') { // this should not happen if default one set to 0 
+    return 'Syntax Error'
+  }
+  if (one !== '' && two === '' && operation !== '') {
+    if (operation !== '+/-' || operation !== 'AC' || operation !== '=') {
+      return 'Syntax Error';
+    }
+    return 'Syntax Error';
+    if (two === '0' && operation === '/') {
+      return 'INF';
     }
   }
-  switch (newOperation) {
-    case '+/-': {
-      if (newNext !== '') {
-        newTotal = operate(total, next, operation) * -1;
-        newNext = null;
-        newOperation = null;
-      }
-      newNext *= -1;
-      newTotal = next;
-      newOperation = 'null';
-      return {
-        newTotal,
-        newNext,
-        operation,
-      };
-    }
-    case 'AC': {
-      newTotal = null;
-      newNext = null;
-      newOperation = null;
-      return {
-        newTotal,
-        newNext,
-        newOperation,
-      };
-    }
-    case '=': {
-      newTotal = operate(total, next, operation);
-      newNext = null;
-      newOperation = null;
-      return {
-        newTotal,
-        newNext,
-        newOperation,
-      };
-    }
-    default: // *-/% as buttonName, assuming operation chaining
-      newTotal = operate(total, next, operation);
-      newNext = null;
-      newOperation = buttonName;
-      break;
-  }
+  return true;
+}
+
+const setTotal = (newTotal, newNext, newOperation) => {
+  let total = newTotal;
+  let next = newNext;
+  let operation = newOperation;
   return {
-    newTotal,
-    newNext,
-    newOperation,
+    total,
+    next,
+    operation
   };
 };
 
-export default calculate;
+const calculate = ({
+    total,
+    next,
+    operation,
+  }, buttonName) => {
+    let result = errorCheck(total, next, operation);
+    if (result != true) {
+      return result;
+    }
+    switch (operation) {
+      case '+/-': {
+        if (next !== '') {
+          result = setTotal((operate(total, next, operation) * -1), null, null)
+          break;
+        }
+        result = setTotal(total*-1, next, null);
+        break;
+         }
+        case 'AC': {
+          result = setTotal(null, null, null);
+          break;
+        }
+        case '=': {
+          result = setTotal(operate(total, next, operation), null, null)
+          break;
+        }
+        default: // *-/% as buttonName, assuming operation chaining (with result output inbetween, not breaking the input)
+        result = setTotal(operate(total, next, operation), null, buttonName);
+        break;
+      }
+      return {
+        result
+      };
+    };
+
+    export default calculate;
